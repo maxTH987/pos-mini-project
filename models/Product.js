98be290eb4ef05a890665fe21bff927c3f455078
Kachen Konchai <kachankk67@gmail.com>
2026-03-28 21:29:56 +0700

    เพิ่ม Schema

--- models/Product.js
+++ models/Product.js
@@ -6,6 +6,7 @@ const productSchema = new mongoose.Schema({
     description: { type: String },
     cost: { type: Number, required: true },
     price: { type: Number, required: true },
+    memberPrice: { type: Number },
     stock: { type: Number, required: true, default: 0 },
     category: { type: String, required: true },
     imageUrl: { type: String, default: 'https://placehold.co/200x200?text=No+Image' }
